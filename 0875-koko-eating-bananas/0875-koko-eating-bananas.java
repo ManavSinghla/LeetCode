d569ebf4f01a11ade26a0deb7555ce3d9f71dc1e
Manav Singla <manavsinghla123@gmail.com>
2025-07-16 20:40:05 +0530

    Time: 26 ms (25.48%), Space: 45 MB (88.72%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -0,0 +1,28 @@
+class Solution {
+    boolean check(int[] piles, int h, int mid){
+        int s=0;
+        for(int i=0;i<piles.length;i++){
+            s+=Math.ceil(piles[i]*1.0/mid);
+        }
+        return s<=h;
+    }
+    public int minEatingSpeed(int[] piles, int h) {
+        int low=1;
+        int high=piles[0];
+        for(int i=1;i<piles.length;i++){
+            high=Math.max(piles[i],high);
+        }
+        int ans=0;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(check(piles,h,mid)){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+}
