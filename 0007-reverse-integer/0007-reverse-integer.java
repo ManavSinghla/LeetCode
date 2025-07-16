ae234c6a4383f63c763329f2186ad3ac15fbdded
Manav Singla <manavsinghla123@gmail.com>
2025-07-17 00:26:05 +0530

    Time: 1 ms (81.95%), Space: 41 MB (57.34%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -2,11 +2,13 @@ class Solution {
     public int reverse(int x) {
         int ans=0;
         while(x!=0){
-            if(ans<Math.pow(-2,31)/10 || ans>(Math.pow(2,31)/10)){
+            if(ans>Math.pow(2,31)/10 || ans<Math.pow(-2,31)/10){
                 return 0;
             }
-            ans=ans*10+x%10;
-            x=x/10;
+            else{
+                ans=ans*10+x%10;
+                x=x/10;
+            }
         }
         return ans;
     }
