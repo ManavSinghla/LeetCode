449469a42f0ec73653f0f41486983b6e8e644cda
Manav Singla <manavsinghla123@gmail.com>
2025-07-16 20:45:29 +0530

    Time: 2 ms (73.32%), Space: 61.8 MB (52.47%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,17 +1,11 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int ans=0;
-        int n=prices.length;
-        int temp=0;
-        int min=prices[0];
-        for(int i=0;i<n;i++){
-            if(temp<(prices[i]-min)){
-                temp=prices[i]-min;
-            }
-            if(prices[i]<min){
-                min=prices[i];
-            }
+        int low=prices[0];
+        for(int i=0;i<prices.length;i++){
+            low=Math.min(low,prices[i]);
+            ans=Math.max(ans,prices[i]-low);
         }
-        return temp;
+        return ans;
     }
 }
