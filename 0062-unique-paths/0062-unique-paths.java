835f1afe8249b9f38b6f62f31054b59bcfb2f7c3
Manav Singla <manavsinghla123@gmail.com>
2025-07-17 12:00:59 +0530

    Time: 0 ms (100%), Space: 40.6 MB (36.16%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] ans=new int[n][m];
+        for(int i=0;i<m;i++){
+            ans[0][i]=1;
+        }
+        for(int i=0;i<n;i++){
+            ans[i][0]=1;
+        }
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                ans[i][j]=ans[i-1][j]+ans[i][j-1];
+            }
+        }
+        return ans[n-1][m-1];
+    }
+}
