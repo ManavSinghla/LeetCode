2810115e5cd4dcfb52b104afaad74081603d0a57
Manav Singla <manavsinghla123@gmail.com>
2025-07-24 09:27:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max=0;
+        int curr=0;
+        for(int i=0;i<nums.length;i++){
+            curr=Math.max(nums[i],curr+nums[i]);
+            max=Math.max(curr,max);
+        }
+        return max;
+    }
+}
