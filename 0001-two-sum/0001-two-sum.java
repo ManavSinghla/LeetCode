2d4d14ae1cf7d14201af64683ff9dfc33ae3f72a
Manav Singla <manavsinghla123@gmail.com>
2025-07-24 10:23:01 +0530

    Time: 4 ms (57.65%), Space: 44.9 MB (78.77%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -4,13 +4,11 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             a.put(nums[i],i);
         }
-        int[] ans=new int[2];
         for(int i=0;i<nums.length;i++){
-            if(a.containsKey(target-nums[i]) && a.get(target-nums[i]) != i){
-                ans[0]=i;
-                ans[1]=a.get(target-nums[i]);
+            if(a.containsKey(target-nums[i]) && i!=a.get(target-nums[i])){
+                return new int[]{i,a.get(target-nums[i])};
             }
         }
-        return ans;
+        return new int[0];
     }
 }
