3e0cb0b47a187d48882b60f5244400c05c4260e6
Manav Singla <manavsinghla123@gmail.com>
2025-08-09 19:01:16 +0530

    Time: 8 ms (45.06%), Space: 40.1 MB (91.55%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,14 +1,9 @@
 class Solution {
+    int fun(int n){
+        if(n==0 || n==1) return n;
+        return fun(n-1)+ fun(n-2);
+    }
     public int fib(int n) {
-        int[] ans=new int[n+1];
-        if(n<2){
-            return n;
-        }
-        ans[0]=0;
-        ans[1]=1;
-        for(int i=2;i<n+1;i++){
-            ans[i]=ans[i-1]+ans[i-2];
-        }
-        return ans[n];
+        return fun(n);
     }
 }
