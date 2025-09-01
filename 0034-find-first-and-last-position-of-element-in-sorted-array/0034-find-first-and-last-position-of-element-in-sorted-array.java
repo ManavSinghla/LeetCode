f0d9b96369f41c4718e860126c6e43dd5956612a
Manav Singla <manavsinghla123@gmail.com>
2025-09-01 18:01:13 +0530

    Time: 0 ms (100%), Space: 46.4 MB (8.97%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,46 +1,38 @@
 class Solution {
-    int firstOccurence(int[]nums,int target){
+    int fOccurence(int[] nums,int target){
         int low=0;
         int high=nums.length-1;
         int ans=-1;
-        while(low<=high){
+        while(high>=low){
             int mid=low+(high-low)/2;
             if(nums[mid]==target){
+                high=mid-1;
                 ans=mid;
-                high=mid-1;
             }
-            else if(nums[mid]>target){
-                high=mid-1;
-            }
-            else{
-                low=mid+1;
-            }
+            else if(nums[mid]<target) low=mid+1;
+            else high=mid-1;
         }
         return ans;
     }
-    int lastOccurence(int[]nums,int target){
+    int lOccurence(int[] nums,int target){
         int low=0;
         int high=nums.length-1;
         int ans=-1;
-        while(low<=high){
+        while(high>=low){
             int mid=low+(high-low)/2;
             if(nums[mid]==target){
+                low=mid+1;
                 ans=mid;
-                low=mid+1;
             }
-            else if(nums[mid]>target){
-                high=mid-1;
-            }
-            else{
-                low=mid+1;
-            }
+            else if(nums[mid]<target) low=mid+1;
+            else high=mid-1;
         }
         return ans;
     }
     public int[] searchRange(int[] nums, int target) {
-        int[] ans=new int[2];
-        ans[0]=firstOccurence(nums,target);
-        ans[1]=lastOccurence(nums,target);
+        int[] ans={-1,-1};
+        ans[0]=fOccurence(nums,target);
+        ans[1]=lOccurence(nums,target);
         return ans;
     }
 }
