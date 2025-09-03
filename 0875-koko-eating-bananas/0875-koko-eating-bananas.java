2a77430c68c7ea764caefebaf7001ca7b206c28e
Manav Singla <manavsinghla123@gmail.com>
2025-09-03 13:26:47 +0530

    Time: 25 ms (35.51%), Space: 45.8 MB (5.11%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,27 +1,22 @@
 class Solution {
-    boolean check(int[]piles, int h,int mid){
-        int sum=0;
+    boolean check(int[] piles, int h, int mid){
+        int s=0;
         for(int i=0;i<piles.length;i++){
-            sum+=Math.ceil(piles[i]*1.0/mid);
+            s+=Math.ceil(piles[i]*1.0/mid);
         }
-        return sum<=h;
+        return s<=h;
     }
     public int minEatingSpeed(int[] piles, int h) {
         int low=1;
-        int high=piles[0];
-        for(int i=0;i<piles.length;i++){
-            high=Math.max(piles[i],high);
-        }
-        int ans=0;
-        while(low<=high){
+        int high=1000000000;
+        int ans=-1;
+        while(high>=low){
             int mid=low+(high-low)/2;
-            if(check(piles,h,mid)){
+            if (check(piles,h,mid)){
                 ans=mid;
                 high=mid-1;
             }
-            else{
-                low=mid+1;
-            }
+            else low=mid+1;
         }
         return ans;
     }
