6f2c7b0aa82da326c5021d36848923e4d9805f68
Manav Singla <manavsinghla123@gmail.com>
2025-09-08 18:15:47 +0530

    Time: 8 ms (45.32%), Space: 40.3 MB (81.57%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,7 +1,7 @@
 class Solution {
     int fun(int n){
         if(n==0 || n==1) return n;
-        return fun(n-1)+ fun(n-2);
+        return fun(n-1)+fun(n-2);
     }
     public int fib(int n) {
         return fun(n);
