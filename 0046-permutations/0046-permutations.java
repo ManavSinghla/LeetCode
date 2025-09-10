1acdec6d91eafaff8756c58d75575386ab9471dd
Manav Singla <manavsinghla123@gmail.com>
2025-09-10 17:39:10 +0530

    Time: 2 ms (34.33%), Space: 44.5 MB (64.65%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,19 +1,19 @@
 class Solution {
-    void fun(int[] nums,List<List<Integer>> res,List<Integer> temp,int start){
-        if(nums.length == temp.size()){
-            res.add(new ArrayList<>(temp));
+    void fun(List<List<Integer>> ans,List<Integer> temp,int[] nums,int start){
+        if(temp.size()==nums.length){
+            ans.add(new ArrayList<>(temp));
         }
         for(int i=0;i<nums.length;i++){
             if(temp.contains(nums[i])) continue;
             temp.add(nums[i]);
-            fun(nums,res,temp,i+1);
+            fun(ans,temp,nums,i+1);
             temp.remove(temp.size()-1);
         }
     }
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> res=new ArrayList<>();
+        List<List<Integer>> ans=new ArrayList<>();
         List<Integer> temp=new ArrayList<>();
-        fun(nums,res,temp,0);
-        return res;
+        fun(ans,temp,nums,0);
+        return ans;
     }
 }
