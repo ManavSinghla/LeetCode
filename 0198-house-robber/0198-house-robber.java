198535f31da087c3d693742053300705a1b9cd62
Manav Singla <manavsinghla123@gmail.com>
2025-09-11 12:33:42 +0530

    Time: 0 ms (100%), Space: 41.5 MB (15.71%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n=nums.length;
+        if(n==1) return nums[0];
+        int[] ans=new int[n];
+        ans[0]=nums[0];
+        ans[1]=Math.max(nums[1],nums[0]);
+        for(int i=2;i<n;i++){
+            ans[i]=Math.max(ans[i-1],ans[i-2]+nums[i]);
+        }
+        return ans[n-1];
+    }
+}
