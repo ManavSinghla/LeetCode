83205f8ea2a5381fc8b3d125d563aedce13fc3bd
Manav Singla <manavsinghla123@gmail.com>
2025-09-11 12:42:50 +0530

    Time: 0 ms (100%), Space: 41.5 MB (18.7%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -2,18 +2,19 @@ class Solution {
     public int rob(int[] nums) {
         int n=nums.length;
         if(n==1) return nums[0];
-        int[] ans=new int[n];
-        int[] ans1=new int[n];
-        ans[0]=nums[0];
-        ans[1]=Math.max(nums[0],nums[1]);
-        ans1[0]=0;
-        ans1[1]=nums[1];
+        if(n==2) return Math.max(nums[0],nums[1]);
+        int[] ans1=new int[n-1];
+        int[] ans2=new int[n-1];
+        ans1[0]=nums[0];
+        ans1[1]=Math.max(nums[0],nums[1]);
+        ans2[0]=nums[1];
+        ans2[1]=Math.max(nums[1],nums[2]);
         for(int i=2;i<n-1;i++){
-            ans[i]=Math.max(ans[i-1],nums[i]+ans[i-2]);
+            ans1[i]=Math.max(ans1[i-1],ans1[i-2]+nums[i]);
         }
-        for(int i=2;i<n;i++){
-            ans1[i]=Math.max(ans1[i-1],nums[i]+ans1[i-2]);
+        for(int i=2;i<n-1;i++){
+            ans2[i]=Math.max(ans2[i-1],ans2[i-2]+nums[i+1]);
         }
-        return Math.max(ans[n-2],ans1[n-1]);
+        return Math.max(ans1[n-2],ans2[n-2]);
     }
 }
