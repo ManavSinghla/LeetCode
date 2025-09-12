b45ea5d9382010762c044c45e2f76295edf49105
Manav Singla <manavsinghla123@gmail.com>
2025-09-12 12:42:14 +0530

    Time: 19 ms (87.29%), Space: 50.9 MB (68.29%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,10 +1,10 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        int m= text1.length();
-        int n=text2.length();
-        int[][] ans=new int[m+1][n+1];
-        for(int i=1;i<=m;i++){
-            for(int j=1;j<=n;j++){
+        int n=text1.length();
+        int m=text2.length();
+        int[][] ans=new int[n+1][m+1];
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
                 if(text1.charAt(i-1)==text2.charAt(j-1)){
                     ans[i][j]=ans[i-1][j-1]+1;
                 }
@@ -13,6 +13,6 @@ class Solution {
                 }
             }
         }
-        return ans[m][n];
+        return ans[n][m];
     }
 }
