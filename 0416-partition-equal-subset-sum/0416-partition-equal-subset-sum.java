fbe6e27ce336941303ccde845341ba07fd1389bb
Manav Singla <manavsinghla123@gmail.com>
2025-09-17 13:47:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,16 +1,19 @@
 class Solution {
     public boolean canPartition(int[] nums) {
+        int n=nums.length;
         int sum=0;
-        for(int i=0;i<nums.length;i++){
+        for(int i=0;i<n;i++){
             sum+=nums[i];
         }
         if(sum%2!=0) return false;
-        int n=nums.length;
         int m=sum/2;
         int[][] ans=new int[n+1][m+1];
-        ans[0][0]=1;
+        // ans[0][0]=1;
+        for(int i=0;i<=n;i++){
+            ans[i][0]=1;
+        }
         for(int i=1;i<=n;i++){
-            for(int j=0;j<=m;j++){
+            for(int j=1;j<=m;j++){
                 if(nums[i-1]>j){
                     ans[i][j]=ans[i-1][j];
                 }
@@ -19,7 +22,7 @@ class Solution {
                 }
             }
         }
-        if(ans[n][m]!=0) return true;
-        return false;
+        System.out.println(ans[n][m]);
+        return ans[n][m]>0;
     }
 }
