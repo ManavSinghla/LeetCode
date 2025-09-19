1f46d04d9a099383ba477286da793c99de52e598
Manav Singla <manavsinghla123@gmail.com>
2025-09-19 11:57:42 +0530

    Time: 1 ms (100%), Space: 44.9 MB (32.8%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,26 +1,29 @@
 class Solution {
-    void dfs(int[][] mat,int[] temp,int i,int j,int r,int c){
-        if(i==r ||j==c ||i<0 || j<0 || mat[i][j]==0) return;
-        mat[i][j]=0;
-        temp[0]+=1;
-        dfs(mat,temp,i,j+1,r,c);
-        dfs(mat,temp,i-1,j,r,c);
-        dfs(mat,temp,i,j-1,r,c);
-        dfs(mat,temp,i+1,j,r,c);
+    void fun(int[][] grid,int i,int j,int r,int c,int[] temp){
+        if(i<0 || j<0 || i>r-1 || j>c-1 || grid[i][j]==0) return;
+        grid[i][j]=0;
+        temp[0]++;
+        fun(grid,i+1,j,r,c,temp);
+        fun(grid,i,j+1,r,c,temp);
+        fun(grid,i-1,j,r,c,temp);
+        fun(grid,i,j-1,r,c,temp);
     }
+    
     public int maxAreaOfIsland(int[][] grid) {
+        int ans=0;
+        int[] temp={0};
         int r=grid.length;
         int c=grid[0].length;
-        int ans=0;
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
-                int[] temp={0};
                 if(grid[i][j]==1){
-                    dfs(grid,temp,i,j,r,c);
+                    fun(grid,i,j,r,c,temp);
                     ans=Math.max(ans,temp[0]);
+                    temp[0]=0;
                 }
             }
         }
         return ans;
+        
     }
 }
