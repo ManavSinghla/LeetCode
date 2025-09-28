21e07d03e654f78237e499bec6777b42b5a9e898
Manav Singla <manavsinghla123@gmail.com>
2025-09-28 23:13:41 +0530

    Time: 0 ms (100%), Space: 46 MB (69.94%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,38 +1,38 @@
 class Solution {
-    int fOccurence(int[] nums,int target){
+    int f(int[] nums, int target) {
         int low=0;
         int high=nums.length-1;
         int ans=-1;
         while(high>=low){
             int mid=low+(high-low)/2;
             if(nums[mid]==target){
-                high=mid-1;
                 ans=mid;
+                high=mid-1;
             }
-            else if(nums[mid]<target) low=mid+1;
-            else high=mid-1;
+            else if(nums[mid]>target) high=mid-1;
+            else low=mid+1;
         }
         return ans;
     }
-    int lOccurence(int[] nums,int target){
+    int l(int[] nums, int target) {
         int low=0;
         int high=nums.length-1;
         int ans=-1;
         while(high>=low){
             int mid=low+(high-low)/2;
             if(nums[mid]==target){
-                low=mid+1;
                 ans=mid;
+                low=mid+1;
             }
-            else if(nums[mid]<target) low=mid+1;
-            else high=mid-1;
+            else if(nums[mid]>target) high=mid-1;
+            else low=mid+1;
         }
         return ans;
     }
     public int[] searchRange(int[] nums, int target) {
         int[] ans={-1,-1};
-        ans[0]=fOccurence(nums,target);
-        ans[1]=lOccurence(nums,target);
+        ans[0]=f(nums,target);
+        ans[1]=l(nums,target);
         return ans;
     }
 }
