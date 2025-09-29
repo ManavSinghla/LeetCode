95e2f65e89f4e950f5c4356e67944afcdf91e661
Manav Singla <manavsinghla123@gmail.com>
2025-09-29 21:29:52 +0530

    Time: 1 ms (45.33%), Space: 57.2 MB (86.94%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -3,7 +3,7 @@ class Solution {
         int n=nums.length;
         int[] ans=new int[n];
         for(int i=0;i<n;i++){
-            ans[(k+i)%n]=nums[i];
+            ans[(i+k)%n]=nums[i];
         }
         for(int i=0;i<n;i++){
             nums[i]=ans[i];
