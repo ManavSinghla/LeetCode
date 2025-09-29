05993f7c89ac08e2af5846712a310d830d5f0fa8
Manav Singla <manavsinghla123@gmail.com>
2025-09-29 21:38:38 +0530

    Time: 3 ms (77.72%), Space: 45.7 MB (45.72%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,19 +1,18 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
         int i=0;
-        while(i<nums.length){
+        int n=nums.length;
+        while(i<n){
             int ci=nums[i]-1;
             if(nums[ci]!=nums[i]){
                 int temp=nums[ci];
                 nums[ci]=nums[i];
                 nums[i]=temp;
             }
-            else{
-                i++;
-            }
+            else i++;
         }
-        int[] ans=new int[2];
-        for(int j=0;j<nums.length;j++){
+        int[] ans={0,0};
+        for(int j=0;j<n;j++){
             if(nums[j]!=j+1){
                 ans[0]=nums[j];
                 ans[1]=j+1;
