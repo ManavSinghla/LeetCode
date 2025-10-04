282043cb71da2d1e33711a16c1ff43ee98167275
Manav Singla <manavsinghla123@gmail.com>
2025-10-04 23:43:20 +0530

    Time: 65 ms (30.24%), Space: 41.2 MB (51.13%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,11 +1,12 @@
 class Solution {
     int fun(int[] nums,int l,int r){
         if(l==r) return nums[l];
-        int pL=nums[l]-fun(nums,l+1,r);
-        int pR=nums[r]-fun(nums,l,r-1);
-        return Math.max(pL,pR);
+        int left=nums[l]-fun(nums,l+1,r);
+        int right=nums[r]-fun(nums,l,r-1);
+        return Math.max(left,right);
     }
     public boolean predictTheWinner(int[] nums) {
-        return fun(nums,0,nums.length-1)>=0;
+        if(fun(nums,0,nums.length-1)>=0) return true;
+        return false;
     }
 }
