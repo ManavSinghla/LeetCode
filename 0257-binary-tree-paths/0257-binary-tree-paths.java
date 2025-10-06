7a511836e3bccad096d9b27d02eb330c38d0bc19
Manav Singla <manavsinghla123@gmail.com>
2025-10-06 12:37:06 +0530

    Time: 2 ms (78.95%), Space: 42.4 MB (92.97%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.java
+++ 0257-binary-tree-paths/0257-binary-tree-paths.java
@@ -14,23 +14,16 @@
  * }
  */
 class Solution {
-    boolean isLeaf(TreeNode root){
-        return root.left==null && root.right==null;
-    }
-
-    void fun(List<String> ans ,String temp,TreeNode root){
+    void fun(List<String> ans,String temp,TreeNode root){
         if(root==null) return;
-        temp=temp+root.val;
-        if(isLeaf(root)){
-            ans.add(temp);
-        }
+        temp+=String.valueOf(root.val);
+        if(root.left==null && root.right==null) ans.add(temp);
         fun(ans,temp+"->",root.left);
         fun(ans,temp+"->",root.right);
     }
     public List<String> binaryTreePaths(TreeNode root) {
         List<String> ans=new ArrayList<>();
-        String temp="";
-        fun(ans,temp,root);
+        fun(ans,"",root);
         return ans;
     }
 }
