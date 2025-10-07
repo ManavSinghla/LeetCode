5a71b36f3fc869e44cbcd25e25a7852704730e6f
Manav Singla <manavsinghla123@gmail.com>
2025-10-07 18:54:56 +0530

    Time: 9 ms (22.64%), Space: 45.3 MB (38.4%) - LeetHub

--- 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.java
+++ 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.java
@@ -14,24 +14,27 @@
  * }
  */
 class Solution {
-    void fun(String[] ans,String temp,TreeNode root){
-        if(root==null) return;
-        int b=(int) 'a';
-        char a=(char) (b+root.val);
-        temp+=a;
-        if(root.left==null && root.right==null){
-            StringBuilder c=new StringBuilder(temp);
-            String d=c.reverse().toString();
-            if(ans[0].equals("") || ans[0].compareTo(d)>0){
-                ans[0]=d;
+    void fun(List<String> ans, String temp, TreeNode root) {
+    if (root == null) return;
+    temp = (char)(root.val + 'a') + temp;  // prepend instead of append
+    if (root.left == null && root.right == null) {
+        ans.add(temp);
+        return;
+    }
+    fun(ans, temp, root.left);
+    fun(ans, temp, root.right);
+}
+
+    public String smallestFromLeaf(TreeNode root) {
+        ArrayList<String> ans=new ArrayList<>();
+        String temp="";
+        fun(ans,temp,root);
+        String finalAns=ans.get(0);
+        for(int i=1;i<ans.size();i++){
+            if(ans.get(i).compareTo(finalAns) <0 ){
+                finalAns=ans.get(i);
             }
         }
-        fun(ans,temp,root.left);
-        fun(ans,temp,root.right);
-    }
-    public String smallestFromLeaf(TreeNode root) {
-        String[] ans={""};
-        fun(ans,"",root);
-        return ans[0];
+        return finalAns;
     }
 }
