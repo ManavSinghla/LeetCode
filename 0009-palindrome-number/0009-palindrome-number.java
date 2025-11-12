29443ed683b0ea99db1fdd8619e8fcaee3c9812e
Manav Singla <manavsinghla123@gmail.com>
2025-11-12 10:18:32 +0530

    Time: 5 ms (66.24%), Space: 45.8 MB (9%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0) return false;
+        int a=0;
+        int b=x;
+        while(b>0){
+            a=a*10+b%10;
+            b=b/10;
+        }
+        if(a==x) return true;
+        return false;
+    }
+}
