5df70bbd73c2675e6d4da8596e9890d751e349ef
Manav Singla <manavsinghla123@gmail.com>
2026-01-22 11:52:37 +0530

    Time: 54 ms (5.25%), Space: 47.6 MB (25.33%) - LeetHub

---
+++ 0845-longest-mountain-in-array/0845-longest-mountain-in-array.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int longestMountain(int[] arr) {
+        int ans=0;
+        int n=arr.length;
+        for(int j=0;j<n;j++){
+            int i=j;
+            int k=i;
+            int temp=1;
+            while(i<n-1){
+                if(arr[i]<arr[i+1]){
+                    i++;
+                    temp++;
+                }
+                else break;
+            }
+            if(i==k || i==n-1 || arr[i]==arr[i+1]) continue;
+            while(i<n-1){
+                if(arr[i]>arr[i+1]){
+                    i++;
+                    temp++;
+                }
+                else break;
+            }
+            if(i==k) continue;
+            ans=Math.max(ans,temp);
+        }
+        return ans;
+    }
+}
