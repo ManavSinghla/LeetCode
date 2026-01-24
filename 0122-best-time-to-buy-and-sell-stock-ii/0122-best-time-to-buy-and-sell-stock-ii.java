092f612c3a372868d76ab7fc4d41e61d5c9aa505
Manav Singla <manavsinghla123@gmail.com>
2026-01-24 12:26:56 +0530

    Time: 1 ms (18.11%), Space: 46.4 MB (14.38%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,11 +1,9 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int sum=0;
+        int ans=0;
         for(int i=0;i<prices.length-1;i++){
-            if(prices[i]<prices[i+1]){
-                sum+=prices[i+1]-prices[i];
-            }
+            if(prices[i]<prices[i+1]) ans+=prices[i+1]-prices[i];
         }
-        return sum;
+        return ans;
     }
 }
