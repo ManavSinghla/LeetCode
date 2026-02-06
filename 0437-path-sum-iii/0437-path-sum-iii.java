d87b82fe62ac035687f624d34ccd3fe94ac15ed1
Manav Singla <manavsinghla123@gmail.com>
2026-02-06 09:41:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0437-path-sum-iii/0437-path-sum-iii.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    void fun(int sum, int[] ans, TreeNode root,int k){
+        if(root==null) return;
+        sum+=root.val;
+        if(sum==k) ans[0]++;
+        fun(sum,ans,root.left,k);
+        fun(sum,ans,root.right,k);
+    }
+    public int pathSum(TreeNode root, int targetSum) {
+        if(targetSum==0) return 0;
+        if(root==null) return 0;
+        int[] ans={0};
+        fun(0,ans,root,targetSum);
+        return ans[0]+pathSum(root.left,targetSum)+pathSum(root.right,targetSum);
+    }
+}
