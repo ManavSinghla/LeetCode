a7a8cd666c735da003d88b79521bc7c6e95ff239
Manav Singla <manavsinghla123@gmail.com>
2026-03-02 22:15:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -5,11 +5,18 @@ class Solution {
         int k=m+n-1;
         while(i>=0 && j>=0){
             if(nums1[i]>nums2[j]){
-                nums1[k--]=nums1[i--];
+                nums1[k]=nums1[i];
+                k--;
+                i--;
             }
             else{
-                nums1[k--]=nums2[j--];
+                nums1[k]=nums2[j];
+                k--;
+                j--;
             }
+        }
+        while(i>=0){
+            nums1[k--]=nums1[i--];
         }
         while(j>=0){
             nums1[k--]=nums2[j--];
