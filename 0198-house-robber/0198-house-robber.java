2a6a72cdda3e40ca18d939451ad52e634a07bbfa
Manav Singla <manavsinghla123@gmail.com>
2026-03-05 15:56:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -4,9 +4,9 @@ class Solution {
         if(n==1) return nums[0];
         int[] ans=new int[n];
         ans[0]=nums[0];
-        ans[1]=Math.max(nums[1],nums[0]);
+        ans[1]=Math.max(nums[0],nums[1]);
         for(int i=2;i<n;i++){
-            ans[i]=Math.max(ans[i-1],ans[i-2]+nums[i]);
+            ans[i]=Math.max(ans[i-2]+nums[i],ans[i-1]);
         }
         return ans[n-1];
     }
