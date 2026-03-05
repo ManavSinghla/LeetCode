3f11aca3ac938b2b4e8f372cb8ce31c60d947ff3
Manav Singla <manavsinghla123@gmail.com>
2026-03-05 15:57:13 +0530

    Time: 3 ms (86.77%), Space: 52.2 MB (67.73%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,20 +1,20 @@
 class Solution {
-    void dfs(char[][] mat,int i,int j,int r,int c){
-        if(i==r || j==c || i<0 || j<0 || mat[i][j]=='0') return;
-        mat[i][j]='0';
-        dfs(mat,i,j+1,r,c);
-        dfs(mat,i-1,j,r,c);
-        dfs(mat,i,j-1,r,c);
-        dfs(mat,i+1,j,r,c);
+    void fun(char[][] grid,int i,int j,int r,int c){
+        if(i<0 || j<0 || i>r-1 || j>c-1 || grid[i][j]=='0') return;
+        grid[i][j]='0';
+        fun(grid,i+1,j,r,c);
+        fun(grid,i,j+1,r,c);
+        fun(grid,i-1,j,r,c);
+        fun(grid,i,j-1,r,c);
     }
     public int numIslands(char[][] grid) {
+        int ans=0;
         int r=grid.length;
         int c=grid[0].length;
-        int ans=0;
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
-                if(grid[i][j] == '1'){
-                    dfs(grid,i,j,r,c);
+                if(grid[i][j]=='1'){
+                    fun(grid,i,j,r,c);
                     ans++;
                 }
             }
