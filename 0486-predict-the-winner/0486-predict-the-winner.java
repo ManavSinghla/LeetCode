eed81cfe4b62ee40f1aa9723779ac781373a968d
Manav Singla <manavsinghla123@gmail.com>
2026-03-07 18:43:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -6,7 +6,6 @@ class Solution {
         return Math.max(left,right);
     }
     public boolean predictTheWinner(int[] nums) {
-        if(fun(nums,0,nums.length-1)>=0) return true;
-        return false;
+        return fun(nums,0,nums.length-1)>=0;
     }
 }
