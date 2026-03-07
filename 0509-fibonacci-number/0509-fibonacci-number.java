9b82255b2d9fac89a8cafb9066b719724f003664
Manav Singla <manavsinghla123@gmail.com>
2026-03-07 18:40:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,10 +1,9 @@
 class Solution {
     public int fib(int n) {
-        if(n<=1) return n;
         int[] ans=new int[n+1];
-        ans[0]=0;
+        if(n<2) return n;
         ans[1]=1;
-        for(int i=2;i<=n;i++){
+        for(int i=2;i<n+1;i++){
             ans[i]=ans[i-1]+ans[i-2];
         }
         return ans[n];
