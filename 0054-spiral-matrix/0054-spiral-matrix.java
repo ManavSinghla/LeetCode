c20f0eed79e7880649d15ebf43855f8b1e4ddb63
Manav Singla <manavsinghla123@gmail.com>
2026-03-15 01:54:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,57 +1,23 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        int n=matrix.length;
-        int m=matrix[0].length;
-        int[][] ans=new int[n][m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                ans[i][j]=101;
-            }
-        }
+        int top=0;
+        int left=0;
+        int bottom=matrix.length-1;
+        int right=matrix[0].length-1;
         List<Integer> a=new ArrayList<>();
-        int i=0;
-        int j=0;
-        while(a.size() < n*m){
-            while(j<m){
-                if(ans[i][j]!=101){
-                    break;
-                }
-                a.add(matrix[i][j]);
-                ans[i][j]=matrix[i][j];
-                j++;
+        while(top<=bottom && left<=right){
+            for(int i=left;i<=right;i++) a.add(matrix[top][i]);
+            top++;
+            for(int i=top;i<=bottom;i++) a.add(matrix[i][right]);
+            right--;
+            if(top<=bottom){
+                for(int i=right;i>=left;i--) a.add(matrix[bottom][i]);
+                bottom--;
             }
-            i++;
-            j--;
-            while(i<n){
-                if(ans[i][j]!=101){
-                    break;
-                }
-                a.add(matrix[i][j]);
-                ans[i][j]=matrix[i][j];
-                i++;
+            if(left<=right){
+                for(int i=bottom;i>=top;i--) a.add(matrix[i][left]);
+                left++;
             }
-            i--;
-            j--;
-            while(j>=0){
-                if(ans[i][j]!=101){
-                    break;
-                }
-                a.add(matrix[i][j]);
-                ans[i][j]=matrix[i][j];
-                j--;
-            }
-            i--;
-            j++;
-            while(i>=0){
-                if(ans[i][j]!=101){
-                    break;
-                }
-                a.add(matrix[i][j]);
-                ans[i][j]=matrix[i][j];
-                i--;
-            }
-            i++;
-            j++;
         }
         return a;
     }
