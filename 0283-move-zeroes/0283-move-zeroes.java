8824e97caa5847340989e4e0ef6d785236b923d1
Manav Singla <manavsinghla123@gmail.com>
2026-04-15 13:32:03 +0530

    Time: 2 ms (91.22%), Space: 47.7 MB (62.72%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,11 +1,15 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int j=0;
         int i=0;
+        int j=0;
         while(i<nums.length){
-            if(nums[i]!=0) nums[j++]=nums[i];
+            if(nums[i]!=0){
+                nums[j++]=nums[i];
+            }
             i++;
         }
-        while(j<nums.length) nums[j++]=0;
+        while(j<nums.length){
+            nums[j++]=0;
+        }
     }
 }
