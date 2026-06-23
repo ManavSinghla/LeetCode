62b9fe8cccd25d8e155c4fd67ce9907baa542011
Manav Singla <manavsinghla123@gmail.com>
2026-06-23 10:33:32 +0530

    Time: 1 ms (98.6%), Space: 47 MB (93.33%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -5,13 +5,13 @@ class Solution {
         for(int i=0;i<n;i++){
             s+=nums[i];
         }
-        int lsum=0;
-        int rsum=s-nums[0];
-        if(lsum==rsum) return 0;
+        int l=0;
+        int r=s-nums[0];
+        if(l==r) return 0;
         for(int i=1;i<n;i++){
-            lsum+=nums[i-1];
-            rsum-=nums[i];
-            if(lsum==rsum) return i;
+            l+=nums[i-1];
+            r-=nums[i];
+            if(l==r) return i;
         }
         return -1;
     }
