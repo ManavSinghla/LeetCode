2571853aaa3adef061e8b068dc32a7e715d26404
Manav Singla <manavsinghla123@gmail.com>
2026-07-12 02:30:06 +0530

    Time: 0 ms (100%), Space: 43.7 MB (82.12%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -5,17 +5,21 @@ class Solution {
         while(h>=l){
             int mid=l+(h-l)/2;
             if(nums[mid]==target) return mid;
-            else if(nums[mid]>=nums[l]){
+            else if(nums[l]<=nums[mid]){
                 if(nums[l]<=target && nums[mid]>target){
                     h=mid-1;
                 }
-                else l=mid+1;
+                else{
+                    l=mid+1;
+                }
             }
             else{
                 if(nums[h]>=target && nums[mid]<target){
                     l=mid+1;
                 }
-                else h=mid-1;
+                else{
+                    h=mid-1;
+                }
             }
         }
         return -1;
