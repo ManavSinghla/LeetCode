9ed15ca3b6473b3d6c70b1e28ba14332bc6cf294
Manav Singla <manavsinghla123@gmail.com>
2026-07-12 02:06:49 +0530

    Time: 1 ms (32.01%), Space: 42.6 MB (28.99%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -1,10 +1,10 @@
 class Solution {
     public int hammingWeight(int n) {
-        int ans=0;
+        int c=0;
         while(n>0){
             n=n&(n-1);
-            ans++;
+            c++;
         }
-        return ans;
+        return c;
     }
 }
