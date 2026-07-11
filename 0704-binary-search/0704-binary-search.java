978923f3a894ad0ccc3cf6617bc6b06405548748
Manav Singla <manavsinghla123@gmail.com>
2026-07-12 02:13:07 +0530

    Time: 0 ms (100%), Space: 48.4 MB (31.38%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -4,9 +4,13 @@ class Solution {
         int h=nums.length-1;
         while(h>=l){
             int mid=l+(h-l)/2;
-            if(nums[mid]==target) return mid;
-            else if(nums[mid]>target) h=mid-1;
-            else l=mid+1;
+            if(target==nums[mid]) return mid;
+            else if(target>=nums[mid]){
+                l=mid+1;
+            }
+            else{
+                h=mid-1;
+            }
         }
         return -1;
     }
