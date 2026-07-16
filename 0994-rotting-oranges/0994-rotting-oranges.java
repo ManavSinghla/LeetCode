a2563c17d6c6fadbb80b2fcca9ecae9c8df12879
Manav Singla <manavsinghla123@gmail.com>
2026-07-16 10:41:09 +0530

    Time: 4 ms (4.89%), Space: 44.1 MB (72.74%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,32 +1,28 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int r=grid.length;
-        int c=grid[0].length;
-        int fresh=0;
-        int rotten=0;
+        int n=grid.length;
+        int m=grid[0].length;
         Queue<Pair<Integer,Integer>> q=new LinkedList<>();
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
+        int fresh=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(grid[i][j]==1) fresh++;
-                else if(grid[i][j]==2){
-                    rotten++;
-                    q.add(new Pair<>(i, j));
-                }
+                if(grid[i][j]==2) q.add(new Pair<>(i,j));
             }
         }
         if(fresh==0) return 0;
-        int ans=0;
         int[] dx={0,0,1,-1};
         int[] dy={1,-1,0,0};
+        int ans=0;
         while(!q.isEmpty()){
             int size=q.size();
             for(int i=0;i<size;i++){
-                Pair<Integer,Integer> node=q.poll();
+                Pair<Integer,Integer> curr=q.poll();
                 for(int j=0;j<4;j++){
-                    int x=node.getKey()+dx[j];
-                    int y=node.getValue()+dy[j];
-                    if(x>=0 && y>=0 && x<r && y<c && grid[x][y]==1){
-                        q.add(new Pair<>(x, y));
+                    int x=curr.getKey()+dx[j];
+                    int y=curr.getValue()+dy[j];
+                    if(x>=0 && y>=0 && x<n && y<m && grid[x][y]==1){
+                        q.add(new Pair<>(x,y));
                         grid[x][y]=2;
                         fresh--;
                     }
