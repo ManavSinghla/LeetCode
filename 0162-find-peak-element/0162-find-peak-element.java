b791fc5113c1200acac4ad76dfad79a6a59d8c93
Manav Singla <manavsinghla123@gmail.com>
2026-07-18 18:31:26 +0530

    Time: 0 ms (100%), Space: 44.2 MB (56.21%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -4,11 +4,11 @@ class Solution {
         int high=nums.length-1;
         while(low<high){
             int mid=low+(high-low)/2;
-            if(nums[mid]<nums[mid+1]){
-                low=mid+1;
+            if(nums[mid]>nums[mid+1]){
+                high=mid;
             }
             else{
-                high=mid;
+                low=mid+1;
             }
         }
         return low;
