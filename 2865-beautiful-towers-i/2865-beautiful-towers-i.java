7d3dffa75b1171fa4491f2d1201b77ac46d6c021
Manav Singla <manavsinghla123@gmail.com>
2026-07-23 12:25:30 +0530

    Time: 8 ms (77.89%), Space: 47.1 MB (13.57%) - LeetHub

---
+++ 2865-beautiful-towers-i/2865-beautiful-towers-i.java
@@ -0,0 +1,41 @@
+class Solution {
+    public long maximumSumOfHeights(int[] heights) {
+        int n = heights.length;
+        Stack<Integer> s = new Stack<>();
+
+        long[] nextSum = new long[n];
+        for (int i = n - 1; i >= 0; i--) {
+            while (!s.isEmpty() && heights[s.peek()] > heights[i]) {
+                s.pop();
+            }
+            if (s.isEmpty()) {
+                nextSum[i] = 1L * (n - i) * heights[i];
+            } else {
+                nextSum[i] = 1L * (s.peek() - i) * heights[i];
+                nextSum[i] += nextSum[s.peek()];
+            }
+            s.push(i);
+        }
+
+        long[] prevSum = new long[n];
+        s.clear();
+        for (int i = 0; i < n; i++) {
+            while (!s.isEmpty() && heights[s.peek()] > heights[i]) {
+                s.pop();
+            }
+            if (s.isEmpty()) {
+                prevSum[i] = 1L * (i + 1) * heights[i];
+            } else {
+                prevSum[i] = 1L * (i - s.peek()) * heights[i];
+                prevSum[i] += prevSum[s.peek()];
+            }
+            s.push(i);
+        }
+
+        long ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans = Math.max(ans, prevSum[i] + nextSum[i] - heights[i]);
+        }
+        return ans;
+    }
+}
