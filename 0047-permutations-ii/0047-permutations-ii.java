0cd013057491a41ecfed6610b5605ebdb065964a
Manav Singla <manavsinghla123@gmail.com>
2026-07-27 14:54:59 +0530

    Time: 3 ms (91.43%), Space: 46.9 MB (30.22%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -6,7 +6,7 @@ class Solution {
         }
         for(int i=0;i<nums.length;i++){
             if(vis[i]) continue;
-            if(i>0 && nums[i]==nums[i-1] && vis[i-1]) continue;
+            if(i>0 && nums[i]==nums[i-1] && !vis[i-1]) continue;
             vis[i]=true;
             temp.add(nums[i]);
             fun(ans,temp,nums,vis);
