bf6ee82bd0a61c1b4f602edc0f65032831b304d1
Manav Singla <manavsinghla123@gmail.com>
2026-07-27 14:31:30 +0530

    Time: 4 ms (28.05%), Space: 46.5 MB (75.74%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    void fun(List<List<Integer>> ans,List<Integer> temp,int[] nums,boolean[] vis){
+        if(temp.size()==nums.length){
+            ans.add(new ArrayList<>(temp));
+            return;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(vis[i]) continue;
+            if(i>0 && nums[i]==nums[i-1] && vis[i-1]) continue;
+            vis[i]=true;
+            temp.add(nums[i]);
+            fun(ans,temp,nums,vis);
+            temp.remove(temp.size()-1);
+            vis[i]=false;
+        }
+    }
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> ans=new ArrayList<>();
+        List<Integer> temp=new ArrayList<>();
+        boolean[] vis=new boolean[nums.length];
+        fun(ans,temp,nums,vis);
+        return ans;
+        
+    }
+}
